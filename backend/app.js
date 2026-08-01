f3529db57e63ded719bd167bc1b95b314658062d
Alberto Cantu <albertocantu@Albertos-MBP.mynet>
2026-08-01 21:14:18 +0100

    Prepare application for production

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,25 @@
 import cors from 'cors'
 import express from 'express'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
 import personsRouter from './routes/personsRoutes.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (_request, response) => {
-  response.send('<h1>Phonebook API</h1>')
-})
-
 app.use('/api/persons', personsRouter)
 
+const frontendDistPath = path.join(__dirname, '../frontend/dist')
+
+app.use(express.static(frontendDistPath))
+
 app.use((error, _request, response, _next) => {
-  console.error(error.message)
+  console.error(error)
 
   if (error.name === 'ValidationError') {
     return response.status(400).json({
@@ -25,6 +30,12 @@ app.use((error, _request, response, _next) => {
   if (error.name === 'CastError') {
     return response.status(400).json({
       error: 'Invalid contact ID',
+    })
+  }
+
+  if (error.code === 11000) {
+    return response.status(400).json({
+      error: 'A contact with this name already exists',
     })
   }
 
